b4b39d940269aa79ca314db98c080db45c35600e
modesnero <modesnero@gmail.com>
2020-03-04 18:29:39 +0300

    add loading indicator logic

---
+++ src/components/loader/loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './loader.css'
+
+const Loader = () => {
+  return (
+    <div className="container-loader">
+      <div className="box1"></div>
+      <div className="box2"></div>
+      <div className="box3"></div>
+    </div>
+  )
+}
+
+export default Loader
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react'
 import ApiClient from '../../api-client'
+import Loader from '../loader'
 
-import loader from './loader.gif'
 import './random-planet.css'
+
+const Content = ({ planet }) => {
+  const { id, name, population, rotationPeriod, diameter } = planet
+  return (
+    <div className='content'>
+      <img
+        className='planet-image'
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+        alt='Planet'
+      />
+      <div>
+        <h4>{name}</h4>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            <span className='term'>Population</span>
+            <span>{population}</span>
+          </li>
+          <li className='list-group-item'>
+            <span className='term'>Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className='list-group-item'>
+            <span className='term'>Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
 export default class RandomPlanet extends Component {
   apiClient = new ApiClient()
 
   state = {
-    planet: {}
+    planet: {},
+    loading: true
   }
 
   constructor () {
@@ -17,48 +48,24 @@ export default class RandomPlanet extends Component {
   }
 
   updatePlanet () {
-    const id = Math.floor(Math.random() * (15 - 1 + 1)) + 1
-    this.apiClient.getPlanet(id).then(planet => this.setState({ planet }))
+    const id = Math.floor(Math.random() * (15 - 2 + 1)) + 2
+    this.apiClient.getPlanet(id).then(planet => {
+      this.setState({ planet, loading: false })
+    })
   }
 
   render () {
-    const {
-      planet: { id, name, population, rotationPeriod, diameter }
-    } = this.state
+    const { planet, loading } = this.state
+    
+    const content = loading ? <Loader /> : <Content planet={planet} />
+
+    let wrapClasses = 'random-planet jumbotron rounded'
+    if (loading) wrapClasses += ' justify-content-center'
+
     return (
-      <div className='container'>
-        <div className='row justify-content-center'>
-          <div className='col'>
-            <img src={loader} />
-          </div>
-        </div>
+      <div className={wrapClasses}>
+        {content}
       </div>
     )
-    /* return (
-      <div className='random-planet jumbotron rounded'>
-        <img
-          className='planet-image'
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-          alt='Planet'
-        />
-        <div>
-          <h4>{name}</h4>
-          <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>
-              <span className='term'>Population</span>
-              <span>{population}</span>
-            </li>
-            <li className='list-group-item'>
-              <span className='term'>Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className='list-group-item'>
-              <span className='term'>Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div>
-      </div>
-    ) */
   }
 }
